ab83fea2384def7095c41970497b33915e9c3232
vale <valexx55@gmail.com>
2023-10-26 09:40:20 +0200

    Diagra Actividad Juego Adivina

--- appinicialprofe/src/appinicialprofe/basico/PruebaAndSencilloYDoble.java
+++ appinicialprofe/src/appinicialprofe/basico/PruebaAndSencilloYDoble.java
@@ -3,10 +3,10 @@ package appinicialprofe.basico;
 public class PruebaAndSencilloYDoble {
 	
 	public static void main(String[] args) {
-		int n = 2&3;
-		boolean resultado = (2==0+1)&&(2+2==4);
-		boolean resultado2 = (2==0+1)&(2+2==4);
-		boolean resultado1 = (1==0+1)||(2+2==4);
+		int n = 2&3;//entre números, es el y logico bit a bit
+		boolean resultado = (2==0+1)&&(2+2==4);//Y AND lógico cortocircuito: si la primera condición no se cumple, no sigue
+		boolean resultado2 = (2==0+1)&(2+2==4);//Y AND lógico normal: aunque la primera condición no se cumple, sigue
+		boolean resultado1 = (1==0+1)||(2+2==4);//O logico OR
 		System.out.println(n);
 		System.out.println(resultado);
 		System.out.println(resultado2);
