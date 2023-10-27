7dc167dddbb7f1d273003801be762559c29aee5f
vale <valexx55@gmail.com>
2023-10-27 10:54:57 +0200

    basicos 1/2

---
+++ appinicialprofe/src/appinicialprofe/basico/MainFunciones.java
@@ -0,0 +1,45 @@
+package appinicialprofe.basico;
+/**
+ * 
+ * LISTA DE EJERCICIOS DE REPASO A LA PARTE DE INICIACIÓN EN JAVA:
+
+1) HACER UN MÉTODO QUE RECIBA UNA CADENA Y UN CARACTER Y DIGA SI ESE CARACTER PERTENECE A LA CADENA O NO 
+2) HACER UN MÉTODO QUE RECIBA UNA CADENA Y UN CARACTER Y DIGA CÚANTAS VECES APARECE ESE CARACTER EN LA CADENA 
+3) HACER UN MÉTODO QUE DADO UN NÚMERO, DIGA SI ES PAR O NO 
+4) HACER UN MÉTODO QUE LE PIDA AL USUARIO SU EDAD Y LE DIGA SI ES MAYOR DE EDAD O NO 
+5) HACER UN MÉTODO QUE RECIBA UNA NOTA DE 0 A 10 Y DIGA SI EQUIVALE A UN APROBADO, BIEN, NOTABLE, O SOBRESALIENTE 
+6) HACER UN MÉTODO QUE RECIBA UNA CADENA Y LA DEVUELVA ALREVÉS 
+7) HACER UN PROGRAMA QUE MUESTRE LA SECUENCIA 3, 6, 9, 12, 15 ...99 
+ *
+ *
+ *	// DEFINO UN MÉTODO public static
+ *	// DARLE UN NOMBRE - camelCase
+ *	// ID la entrada -
+ *	// ID la salida -
+ *
+ *	//pensar los pasos en ESPAÑOL en psuedocódigo
+ */
+public class MainFunciones {
+	
+	public static void main(String[] args) {
+		//TODO llamar/invocar a las funciones
+		boolean resultado = esNumeroPar(8);
+		System.out.println(resultado);
+	}
+	//EN TOTAL DEBO TENER 8 MÉTODOS
+	
+	//3) HACER UN MÉTODO QUE DADO UN NÚMERO, DIGA SI ES PAR O NO
+	public static boolean esNumeroPar (int numero)
+	{
+		boolean espar = false;
+		
+			//TODO completar el código
+		
+		return espar;
+	}
+	
+	
+	
+	
+
+}
