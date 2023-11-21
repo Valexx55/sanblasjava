ee5802050b6b6bcf1169e31331b4de02e5f620aa
vale <valexx55@gmail.com>
2023-11-21 13:51:15 +0100

    excepciones2

--- appinicialprofe/src/appinicialprofe/excepciones/MainExceptions.java
+++ appinicialprofe/src/appinicialprofe/excepciones/MainExceptions.java
@@ -3,12 +3,27 @@ package appinicialprofe.excepciones;
 public class MainExceptions {
 	
 	public static void main(String[] args) {
+		
 		System.out.println("INICIO");
 		String saludo = "HOLA";
 		String saludo1 = null;
-		//saludo.charAt(4);
-		saludo1.length();
-		
+		try {
+			saludo.charAt(4);
+			saludo1.length();
+			
+		}
+		catch (IndexOutOfBoundsException e) {
+			System.err.println("FALLO Index ");
+			e.printStackTrace();
+		}
+		catch (Exception e) {
+			System.err.println("FALLO Exception ");
+			e.printStackTrace();
+		}catch (Throwable e) {
+			System.err.println("FALLO Throwable ");
+			e.printStackTrace();
+		}
+				
 		System.out.println("FIN");
 	}
 
